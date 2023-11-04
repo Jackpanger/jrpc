a63782f267d5cabc7fec099c9d9956cbf8d1fa8d
Jack Pang <jp6862@nyu.edu>
2023-11-03 22:16:34 -0400

    update pom.xml and remove .idea file

---
+++ jrpc-manager/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.jackpang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
